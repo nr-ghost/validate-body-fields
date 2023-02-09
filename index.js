b48b8266e73189ea93e08b1fbc5aeb61e01836e1
nero <79690698+nerosengtianthr@users.noreply.github.com>
2023-02-09 19:05:39 +0700

    version 1.0.6

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 function validateFields(validateFields, formData) {
   const formKeys = Object.keys(formData);
-  let results = [];
+  let logs = [];
   for (let i = 0; i < validateFields.length; i++) {
     let validateField = validateFields[i];
     const requiredField = validateFields[i].includes("!");
+    const requiredType = validateFields[i].includes(":");
+
+    // Validate Types
+    if (requiredType) {
+      const key = validateField.split(":")[0].replace(/[^a-zA-Z0-9 ]/g, "");
+      const type = validateField.split(":")[1].toLowerCase();
+      const form_type = formData[key];
+      if (type !== typeof form_type) {
+        logs.push(`Required Type: [${type}] ${key}`);
+      }
+      validateField = validateFields[i].split(":")[0];
+    }
 
     // Validate Require
     if (requiredField) {
-      let key = validateFields[i].split("!")[1];
+      let key = validateField.split("!")[1];
       if (!formKeys.includes(key) || formData[key] === "") {
-        results.push(`Require Value: ${validateFields[i].split("!")[1]}`);
+        logs.push(`Required Value: ${key}`);
       }
-      validateField = validateFields[i].split("!")[1];
+      validateField = key;
     }
 
     // Validate Matching Key Field
     if (!formKeys.includes(validateField)) {
-      results.push(`Required Field: ${validateField}`);
+      logs.push(`Required Field: ${validateField}`);
     }
   }
-  return results.length > 0 ? [true, results] : [false, null];
+  return logs.length > 0 ? [true, logs] : [false, null];
 }
 
-module.exports = validateFields;
+module.exports = { validateFields };
