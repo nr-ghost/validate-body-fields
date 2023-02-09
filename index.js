0d214930287f536092a310b4683aa5eb557aac59
nero <79690698+nerosengtianthr@users.noreply.github.com>
2023-02-09 15:41:22 +0700

    packet

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ function validateFields(validateFields, formData) {
   const formKeys = Object.keys(formData);
   let results = [];
   for (let i = 0; i < validateFields.length; i++) {
-    if (!formKeys.includes(validateFields[i])) results.push(validateFields[i]);
+    let validateField = validateFields[i];
+    const requiredField = validateFields[i].includes("!");
+
+    // Validate Require
+    if (requiredField) {
+      let key = validateFields[i].split("!")[1];
+      if (!formKeys.includes(key) || formData[key] === "") {
+        results.push(`Require Value: ${validateFields[i].split("!")[1]}`);
+      }
+      validateField = validateFields[i].split("!")[1];
+    }
+
+    // Validate Matching Key Field
+    if (!formKeys.includes(validateField)) {
+      results.push(`Required Field: ${validateField}`);
+    }
   }
   return results.length > 0 ? [true, results] : [false, null];
 }
